6adfb3f920d70796eb0a62229415a0a6e4ce7220
vince87 <vincenzo.bini@gmail.com>
2020-03-25 15:43:40 +0100

    Update menu.sh

--- script/menu.sh
+++ script/menu.sh
@@ -8,12 +8,10 @@ MENU="Scegliere un opzione:"
 
 OPTIONS=(1 "Aggiorna Script"
          2 "Abilita/Disabilita controlli analogici"
-         3 "Abilita/Disabilita Joystick"
-         4 "Abilita/Disabilita JammaPi"
-         5 "Abilita/Disabilita Runcommand RetroPie"
-         6 "Switch JAMMA 15Khz/HDMI"
-         7 "Abilita/Disabilita Kiosk Mode"
-         8 "Lancia script richiesto")
+         3 "Abilita/Disabilita Runcommand RetroPie"
+         4 "Switch JAMMA 15Khz/HDMI"
+         5 "Abilita/Disabilita Kiosk Mode"
+         6 "Lancia script richiesto")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -27,8 +25,6 @@ clear
 case $CHOICE in
         1) bash ~/JammaPi/script/updatescript.sh ;;
         2) bash ~/JammaPi/script/analogjs.sh ;;
-        3) bash ~/JammaPi/script/joystickonoff.sh ;;
-        4) bash ~/JammaPi/script/jammapionoff.sh ;;
         5) bash ~/JammaPi/script/runcommandonoff.sh ;;
         6) bash ~/JammaPi/script/switchvideo.sh ;;
         7) bash ~/JammaPi/script/kiosk.sh ;;
