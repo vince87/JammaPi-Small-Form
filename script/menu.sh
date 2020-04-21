c395c9a00ac7c70a2763b7bce10168447b2ae5ff
vince87 <vincenzo.bini@gmail.com>
2020-04-21 23:26:22 +0200

    Update menu.sh

--- script/menu.sh
+++ script/menu.sh
@@ -23,7 +23,7 @@ clear
 case $CHOICE in
         1) bash ~/JammaPi-Small-Form/script/updatescript.sh ;;
         2) bash ~/JammaPi-Small-Form/script/switchvideo.sh ;;
-        2) bash ~/JammaPi-Small-Form/script/audio.sh ;;
+        3) bash ~/JammaPi-Small-Form/script/audio.sh ;;
         99)
         if test -f "/boot/personalizzazione.sh"; then
           echo "$FILE esite!"
